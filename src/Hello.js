9c49c904d033a249805a283833c55669b23c7763
Pavan Baloju <pavanbaloju9@gmail.com>
2023-01-05 11:22:30 +0530

    Import tachyons and add props

--- src/Hello.js
+++ src/Hello.js
@@ -1,15 +1,12 @@
-import { Component } from "react";
 import './Hello.css';
 
-class Hello extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Hello World</h1>
-                <h2>Welcome to react</h2>
-            </div>
-        )
-    }
+const Hello = props => {
+    return (
+        <div className="f1 tc">
+            <h1>Hello World</h1>
+            <h2>{props.greeting}</h2>
+        </div>
+    );
 }
 
 export default Hello;
