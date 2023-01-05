e451ff2798086ac952aa097f127f621a987ae69b
Pavan Baloju <pavanbaloju9@gmail.com>
2023-01-05 11:27:06 +0530

    Remove hello and app components

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 import 'tachyons';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hello greeting={'Hello React Ninja'}/>
+    <h1>Hello</h1>
   </React.StrictMode>
 );
 
